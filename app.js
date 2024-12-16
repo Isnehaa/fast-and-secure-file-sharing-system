923cced118d6ecf5a8465803382a42c8539d2927
Isnehaa <snehapatil3106@gmail.com>
2024-12-16 16:26:44 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FileUpload from './FileUpload';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Fast and Secure File Sharing</h1>
+      <FileUpload />
+    </div>
+  );
+};
+
+export default App;
